e83569beb6143a559bad959fbb955e3bbec97576
Eihapkaram <eihapkaram9090@gmail.com>
2025-09-18 06:29:54 -0700

    Update AuthServiceProvider.php
    ن

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,31 +7,23 @@ use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->registerPolicies();
 
-        // Register Passport routes
-        Passport::routes();
+        // Use keys from environment variables
+        if (env('PASSPORT_PRIVATE_KEY') && env('PASSPORT_PUBLIC_KEY')) {
+            Passport::loadKeysFrom([
+                'private' => env('PASSPORT_PRIVATE_KEY'),
+                'public'  => env('PASSPORT_PUBLIC_KEY'),
+            ]);
+        }
 
-        // Load keys from environment variables
-        Passport::loadKeysFrom([
-            'private' => env('PASSPORT_PRIVATE_KEY'),
-            'public'  => env('PASSPORT_PUBLIC_KEY'),
-        ]);
+        // Optional: disable routes registration if not needed
+        // Passport::ignoreRoutes();
     }
 }
