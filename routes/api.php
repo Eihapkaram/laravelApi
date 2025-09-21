ca891e7e7e12e6aa2fc48b1b7eb8f08d929838e1
Eihapkaram <eihapkaram9090@gmail.com>
2025-09-21 06:34:09 -0700

    Update api.php
    up

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\AddToController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CategorieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +22,23 @@ use App\Http\Controllers\UserController;
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'Login']);
 Route::get('pro',[ProductController::class, 'index']);
-Route::post('create',[ProductController::class, 'create']);
-Route::middleware(['auth:api'])->group(function () {
-    Route::get('usersinfo', [UserController::class, 'userinfo'])->name('userinfo');
-    Route::get('show/{id}',[ProductController::class, 'show']);
+Route::get('show/{id}',[ProductController::class, 'show']);
+Route::get('categorie/proshow', [CategorieController::class, 'showCateProduct']);
 
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('create',[ProductController::class, 'create']);
+    Route::post('cart/add', [AddToController::class, 'addfun']);
+     Route::post('categorie/add', [CategorieController::class, 'AddCate']);
+Route::get('cart/show', [AddToController::class, 'CartShow']);
+    Route::delete('cart/deleteAll', [AddToController::class, 'deleteAllCartItems']);
+    Route::post('order/add', [OrderController::class, 'createOrder']);
+    Route::post('order/show', [OrderController::class, 'showOrder']);
+    Route::post('order/show/latest', [OrderController::class, 'showlatestOrder']);
+    Route::delete('order/delete/all', [OrderController::class, 'deleteAllOrder']);
+    Route::get('usersinfo', [UserController::class, 'userinfo'])->name('userinfo');
     Route::post('update/{id}',[ProductController::class, 'update']);
     Route::delete('destroy/{id}',[ProductController::class, 'destroy']);
+     Route::delete('order/delete/{id}', [OrderController::class, 'deleteOrder']);
+     Route::delete('cart/delete/{id}', [AddToController::class, 'deleteCartItem']);
+
 });
