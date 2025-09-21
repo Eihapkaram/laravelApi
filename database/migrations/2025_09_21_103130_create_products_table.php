8baf023421a4a1ac4bc37e102d15fd63770d6fed
Eihapkaram <eihapkaram9090@gmail.com>
2025-09-21 06:29:23 -0700

    up
    up

---
+++ database/migrations/2025_09_21_103130_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('titel', 200);
+            $table->longText('description');
+            $table->smallInteger('votes');
+            $table->string('url');
+            $table->string('img')->nullable();
+            $table->string('images_url')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
