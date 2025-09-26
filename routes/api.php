e28b308d6a40a8a82ce0d789503935c92f08f457
Eihapkaram <eihapkaram9090@gmail.com>
2025-09-26 09:23:51 -0700

    Update api.php
    hn

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\Http\Controllers\AddToController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\AddToController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\CategorieController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\PageController;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Storage;
 
 /*
@@ -24,19 +24,22 @@ use Illuminate\Support\Facades\Storage;
 */
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
+
     return response()->json(['message' => 'Email verified successfully']);
 })->middleware(['auth:api', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
+
     return response()->json(['message' => 'Verification link sent!']);
 })->middleware(['auth:api', 'throttle:6,1'])->name('verification.send');
 
-
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'Login'])->name('login');
-Route::get('pro',[ProductController::class, 'index']);
-Route::get('show/{id}',[ProductController::class, 'show']);
+Route::get('pro', [ProductController::class, 'index']);
+Route::get('pageProducts/show', [PageController::class, 'showPageProduct']);
+Route::get('categorie/show', [CategorieController::class, 'showCateProduct']);
+Route::get('show/{id}', [ProductController::class, 'show']);
 Route::get('categorie/proshow', [CategorieController::class, 'showCateProduct']);
 Route::get('show/reviwe/{id}', [ReviewController::class, 'showProReviwes']);
 Route::get('/products/{filename}', function ($filename) {
@@ -46,56 +49,51 @@ Route::get('/products/{filename}', function ($filename) {
     // منع الأحرف الغير مسموح بها
     $filename = preg_replace('/[^A-Za-z0-9\-\_\.]/', '_', $filename);
 
-    $path = 'products/' . $filename;
+    $path = 'products/'.$filename;
 
-    if (!Storage::disk('public')->exists($path)) {
+    if (! Storage::disk('public')->exists($path)) {
         return response()->json([
-            'message' => 'الصورة غير موجودة'
+            'message' => 'الصورة غير موجودة',
         ], 404);
     }
 
-    return response()->file(storage_path('app/public/' . $path));
+    return response()->file(storage_path('app/public/'.$path));
 })->where('filename', '.*');
-
 
 Route::middleware('auth:api')->group(function () {
     Route::post('logout', [UserController::class, 'logout']);
-    Route::post('logoutFromAll', [UserController::class, 'logoutAll']);});
-
-
-
-    Route::middleware(['auth:api','UserRole'])->group(function () {
-Route::prefix('dashboard')->group(function () {
-    Route::post('create',[ProductController::class, 'create']);
-     Route::post('categorie/add', [CategorieController::class, 'AddCate']);
-      Route::post('page/add', [PageController::class, 'AddPage']);
-    Route::get('pageProducts/show', [PageController::class, 'showPageProduct']);
-    Route::delete('page/Delete/{id}', [PageController::class, 'DeletePage']);
-     Route::get('categorie/show', [CategorieController::class, 'showCateProduct']);
-      Route::get('usersinfo', [UserController::class, 'userinfo'])->name('userinfo');
-    Route::post('update/{id}',[ProductController::class, 'update']);
-    Route::delete('destroy/{id}',[ProductController::class, 'destroy']);
-    Route::delete('categorie/{id}', [CategorieController::class, 'DeleteCate']);
-    Route::post('categorie/update/{id}', [CategorieController::class, 'UpdateCate']);
-    Route::post('page/Update/{id}', [PageController::class, 'UpdatePage']);
-    Route::post('categorie/update/{id}', [CategorieController::class, 'UpdateCate']);
-     Route::delete('categorie/delete/{id}', [CategorieController::class, 'DeleteCate']);
-      Route::delete('user/delete/{id}', [UserController::class, 'UserDelete']);
+    Route::post('logoutFromAll', [UserController::class, 'logoutAll']);
 });
 
+Route::middleware(['auth:api', 'UserRole'])->group(function () {
+    Route::prefix('dashboard')->group(function () {
+        Route::post('create', [ProductController::class, 'create']);
+        Route::post('categorie/add', [CategorieController::class, 'AddCate']);
+        Route::post('page/add', [PageController::class, 'AddPage']);
 
-Route::post('cart/add', [AddToController::class, 'addfun']);
-Route::get('cart/show', [AddToController::class, 'CartShow']);
+        Route::delete('page/Delete/{id}', [PageController::class, 'DeletePage']);
+
+        Route::get('usersinfo', [UserController::class, 'userinfo'])->name('userinfo');
+        Route::post('update/{id}', [ProductController::class, 'update']);
+        Route::delete('destroy/{id}', [ProductController::class, 'destroy']);
+        Route::delete('categorie/{id}', [CategorieController::class, 'DeleteCate']);
+        Route::post('categorie/update/{id}', [CategorieController::class, 'UpdateCate']);
+        Route::post('page/Update/{id}', [PageController::class, 'UpdatePage']);
+        Route::post('categorie/update/{id}', [CategorieController::class, 'UpdateCate']);
+        Route::delete('categorie/delete/{id}', [CategorieController::class, 'DeleteCate']);
+        Route::delete('user/delete/{id}', [UserController::class, 'UserDelete']);
+    });
+
+    Route::post('cart/add', [AddToController::class, 'addfun']);
+    Route::get('cart/show', [AddToController::class, 'CartShow']);
     Route::delete('cart/deleteAll', [AddToController::class, 'deleteAllCartItems']);
     Route::post('order/add', [OrderController::class, 'createOrder']);
     Route::post('order/show', [OrderController::class, 'showOrder']);
     Route::post('order/show/latest', [OrderController::class, 'showlatestOrder']);
     Route::delete('order/delete/all', [OrderController::class, 'deleteAllOrder']);
-     Route::delete('order/delete/{id}', [OrderController::class, 'deleteOrder']);
-     Route::delete('cart/delete/{id}', [AddToController::class, 'deleteCartItem']);
-     Route::post('add/reviweForProdict/{id}', [ReviewController::class, 'AddReviwes']);
-     Route::post('update/reviwe/{id}', [ReviewController::class, 'UpdateReviwes']);
-     Route::delete('delete/reviwe/{id}/{reviweid}', [ReviewController::class, 'DeleteReviwes']);
-
-
+    Route::delete('order/delete/{id}', [OrderController::class, 'deleteOrder']);
+    Route::delete('cart/delete/{id}', [AddToController::class, 'deleteCartItem']);
+    Route::post('add/reviweForProdict/{id}', [ReviewController::class, 'AddReviwes']);
+    Route::post('update/reviwe/{id}', [ReviewController::class, 'UpdateReviwes']);
+    Route::delete('delete/reviwe/{id}/{reviweid}', [ReviewController::class, 'DeleteReviwes']);
 });
