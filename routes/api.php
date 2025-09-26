b30554cfec66e6c42d8fe2e3cea3cf591054a9a8
Eihapkaram <eihapkaram9090@gmail.com>
2025-09-26 16:37:48 -0700

    Update api.php
    s

--- routes/api.php
+++ routes/api.php
@@ -58,7 +58,21 @@ Route::get('/products/{filename}', function ($filename) {
         ], 404);
     }
 
-    return response()->file(storage_path('app/public/'.$path));
+    // فك الترميز من الرابط
+    $filename = urldecode($filename);
+
+    // منع الأحرف الغير مسموح بها
+    $filename = preg_replace('/[^A-Za-z0-9\-\_\.]/', '_', $filename);
+
+    $path2 = 'users/'.$filename;
+
+    if (! Storage::disk('public')->exists($path)) {
+        return response()->json([
+            'message' => 'الصورة غير موجودة',
+        ], 404);
+    }
+
+    return response()->file(storage_path('app/public/'.$path2));
 })->where('filename', '.*');
 
 Route::middleware('auth:api')->group(function () {
