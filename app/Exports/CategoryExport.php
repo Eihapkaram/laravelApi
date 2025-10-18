060c81db1127f2574cafbc7ec0d920787a2345b1
Eihapkaram <eihapkaram9090@gmail.com>
2025-10-18 09:27:39 -0700

    Update CategoryExport.php
    ة

--- app/Exports/CategoryExport.php
+++ app/Exports/CategoryExport.php
@@ -2,13 +2,39 @@
 
 namespace App\Exports;
 
-use App\Models\categorie;
+use App\Models\Categorie;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class CategoryExport implements FromCollection
+class CategoryExport implements FromCollection, WithHeadings, WithMapping
 {
     public function collection()
     {
-        return categorie::all(['id', 'name', 'slug', 'description', 'img', 'created_at']);
+        return Categorie::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Name',
+            'Slug',
+            'Description',
+            'Image',
+            'Created At',
+        ];
+    }
+
+    public function map($category): array
+    {
+        return [
+            $category->id,
+            $category->name,
+            $category->slug,
+            $category->description,
+            $category->img,
+            $category->created_at ? $category->created_at->format('Y-m-d H:i:s') : '',
+        ];
     }
 }
