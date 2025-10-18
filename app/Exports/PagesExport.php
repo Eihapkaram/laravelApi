3ec1f2282e40cd07fff6b85299979b82769fafc4
Eihapkaram <eihapkaram9090@gmail.com>
2025-10-18 09:24:27 -0700

    Update PagesExport.php
    ة

--- app/Exports/PagesExport.php
+++ app/Exports/PagesExport.php
@@ -4,12 +4,42 @@ namespace App\Exports;
 
 use App\Models\Page;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class PagesExport implements FromCollection
+class PagesExport implements FromCollection, WithHeadings, WithMapping
 {
+    /**
+     * إرجاع البيانات
+     */
     public function collection()
     {
-        // هيجيب كل الصفحات من قاعدة البيانات
         return Page::all();
+    }
+
+    /**
+     * العناوين في أول صف
+     */
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Slug',
+            'Created At',
+            'Updated At',
+        ];
+    }
+
+    /**
+     * تحديد كيفية كتابة كل صف في Excel
+     */
+    public function map($page): array
+    {
+        return [
+            $page->id,
+            $page->slug,
+            $page->created_at ? $page->created_at->format('Y-m-d H:i:s') : '',
+            $page->updated_at ? $page->updated_at->format('Y-m-d H:i:s') : ''
+        ];
     }
 }
