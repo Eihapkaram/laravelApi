1b9cb2b54479918141b601cb901bb2f1a5b8fd77
Eihapkaram <eihapkaram9090@gmail.com>
2025-10-18 09:26:26 -0700

    Update OrdersExport.php
    ة

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -5,52 +5,58 @@ namespace App\Exports;
 use App\Models\Order;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class OrdersExport implements FromCollection, WithHeadings
+class OrdersExport implements FromCollection, WithHeadings, WithMapping
 {
     public function collection()
     {
-        return Order::select(
-            'id',
-            'user_id',
-            'seller_id',
-            'store_name',
-            'status',
-            'total_price',
-            'shipping_address',
-            'payment_method',
-            'store_banner',
-            'approval_status',
-            'approved_at',
-            'city',
-            'governorate',
-            'street',
-            'phone',
-            'created_at',
-            'updated_at'
-        )->get();
+        return Order::all();
     }
 
     public function headings(): array
     {
         return [
-            'id',
-            'user_id',
-            'seller_id',
-            'store_name',
-            'status',
-            'total_price',
-            'shipping_address',
-            'payment_method',
-            'store_banner',
-            'approval_status',
-            'approved_at',
-            'city',
-            'governorate',
-            'street',
-            'phone',
-            'created_at',
-            'updated_at',
+            'ID',
+            'User ID',
+            'Seller ID',
+            'Store Name',
+            'Status',
+            'Total Price',
+            'Shipping Address',
+            'Payment Method',
+            'Store Banner',
+            'Approval Status',
+            'Approved At',
+            'City',
+            'Governorate',
+            'Street',
+            'Phone',
+            'Created At',
+            'Updated At',
+        ];
+    }
+
+    public function map($order): array
+    {
+        return [
+            $order->id,
+            $order->user_id,
+            $order->seller_id,
+            $order->store_name,
+            $order->status,
+            $order->total_price,
+            $order->shipping_address,
+            $order->payment_method,
+            $order->store_banner,
+            $order->approval_status,
+            $order->approved_at ? $order->approved_at->format('Y-m-d H:i:s') : '',
+            $order->city,
+            $order->governorate,
+            $order->street,
+            "'".$order->phone, // نجمة صغيرة لجعل الرقم كنص
+            $order->created_at ? $order->created_at->format('Y-m-d H:i:s') : '',
+            $order->updated_at ? $order->updated_at->format('Y-m-d H:i:s') : '',
         ];
     }
 }
