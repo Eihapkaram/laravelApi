6f84d61a87892d3a7f249344bb74972efacafb9d
Eihapkaram <eihapkaram9090@gmail.com>
2025-11-13 08:35:11 -0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,9 +30,9 @@ class UserController extends Controller
             'longitude' => 'required|numeric',
             'security_question'  => 'required',
             'security_answer'  => 'required',
-            'wallet_number'  => 'required|numeric',
-            'front_id_image'  => 'required',
-            'back_id_image'  => 'required',
+            'wallet_number'  => 'nullable|numeric',
+            'front_id_image'  => 'nullable',
+            'back_id_image'  => 'nullable',
         ]);
 
         if ($request->hasFile('img')) {
@@ -283,9 +283,9 @@ class UserController extends Controller
             'role' => 'required',
             'security_question' => 'required',
             'security_answer' => 'required',
-            'wallet_number' => 'required|numeric',
-            'front_id_image' => 'required',
-            'back_id_image' => 'required',
+            'wallet_number' => 'nullable|numeric',
+            'front_id_image' => 'nullable',
+            'back_id_image' => 'nullable',
             'phone' => [
                 'required',
                 'unique:users',
